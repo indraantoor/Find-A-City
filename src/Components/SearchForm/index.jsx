a5469aa1fac40156ecef061764fd946c901dcd91
Indraan S. Toor <indraantoor@gmail.com>
2022-04-01 16:04:08 +0530

    Add: reducer pattern

--- src/Components/SearchForm/index.jsx
+++ src/Components/SearchForm/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import ResultsList from "../ResultsList";
 import SearchIcon from "../../Assets/SearchIcon";
 import ClearIcon from "../../Assets/ClearIcon";
@@ -10,6 +10,35 @@ import {
   SearchFormContainer,
   SearchInput,
 } from "./style";
+
+const initialState = {
+  searchText: "",
+  places: {},
+};
+
+const SET_SEARCH_TEXT = "SET_SEARCH_TEXT";
+const SET_PLACES = "SET_PLACES";
+
+const reducer = (state, action) => {
+  console.log({ action });
+  if (action.type === SET_SEARCH_TEXT) {
+    const { searchText } = action.payload;
+    return {
+      ...state,
+      searchText,
+    };
+  }
+
+  if (action.type === SET_PLACES) {
+    const { places } = action.payload;
+    return {
+      ...state,
+      places,
+    };
+  }
+
+  return state;
+};
 
 const getPlaces = (debouncedSearchText, setFn) => {
   let cancelled = false;
@@ -34,21 +63,50 @@ const SearchForm = () => {
   const [searchText, setSearchText] = useState("");
   const debouncedSearchText = useDebounce(searchText, 180);
 
-  useEffect(
-    () => getPlaces(debouncedSearchText, setPlaces),
-    [debouncedSearchText]
-  );
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const placesSet = (data) => {
+    dispatch({
+      type: SET_PLACES,
+      payload: { places: data },
+    });
+  };
+
+  useEffect(() => {
+    getPlaces(debouncedSearchText, setPlaces);
+  }, [debouncedSearchText]);
+
+  useEffect(() => {
+    placesSet(places);
+  }, [places]);
+
+  const setText = (searchText) => {
+    dispatch({
+      type: SET_SEARCH_TEXT,
+      payload: {
+        searchText,
+      },
+    });
+  };
+
+  const clearText = () => {
+    setText("");
+  };
 
   const handleChange = (e) => {
     const updatedSearchText = e.target.value;
     setSearchText(updatedSearchText);
+    setText(updatedSearchText);
   };
 
   const handleClick = (e) => {
     // Clear Textbox
     const updatedSearchText = "";
     setSearchText(updatedSearchText);
+    clearText();
   };
+
+  console.log(state);
 
   return (
     <SearchContainer>
