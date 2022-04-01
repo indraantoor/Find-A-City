ee378bc2affb6924f8c6b3f42f69c0a873bae788
Indraan S. Toor <indraantoor@gmail.com>
2022-04-01 12:01:43 +0530

    Refactor: separate style from header component

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,11 @@
+import { HeaderContainer, LogoText } from "./style";
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <LogoText>FindCity.com</LogoText>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ src/Components/Header/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  background-color: #030301;
+  height: 60px;
+  color: white;
+  display: flex;
+  // justify-content: center;
+  align-items: center;
+`;
+
+export const LogoText = styled.h1`
+  margin-left: 40px;
+  cursor: pointer;
+`;
