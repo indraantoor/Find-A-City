c33db9c070cb9b20f1ab52786ecdbe0deb5494ce
Indraan S. Toor <indraantoor@gmail.com>
2022-04-01 11:57:54 +0530

    Refactor: separate style from footer component

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,12 @@
+import { FooterContainer, CopyrightText } from "./style";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <CopyrightText>(C) Copyright 2022 - Indraan S Toor</CopyrightText>
+      <CopyrightText>Built By - Indraan S Toor</CopyrightText>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
--- src/Components/Footer/style.js
+++ src/Components/Footer/style.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const FooterContainer = styled.footer`
+export const FooterContainer = styled.footer`
   // background-color: #001011;
   background-color: #030301;
   // background-color: rgba(0, 16, 17, 0.8);
@@ -14,21 +14,10 @@ const FooterContainer = styled.footer`
   color: #757780;
 `;
 
-const CopyrightText = styled.p`
+export const CopyrightText = styled.p`
   // padding: 10px;
   font-size: 14px;
   font-weight: 300;
   margin: 40px;
   // text-align: center;
 `;
-
-const Footer = () => {
-  return (
-    <FooterContainer>
-      <CopyrightText>(C) Copyright 2022 - Indraan S Toor</CopyrightText>
-      <CopyrightText>Built By - Indraan S Toor</CopyrightText>
-    </FooterContainer>
-  );
-};
-
-export default Footer;
