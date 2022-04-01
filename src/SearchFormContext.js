0ec071a35454f49cc1ccef416556f2f9a8fa26a7
Indraan S. Toor <indraantoor@gmail.com>
2022-04-01 16:32:49 +0530

    Add: context api config

---
+++ src/SearchFormContext.js
@@ -0,0 +1,64 @@
+import React, { useReducer, createContext, useCallback } from "react";
+
+const initialState = {
+  searchText: "",
+  places: {},
+};
+
+export const SearchFormContext = createContext();
+
+const SET_SEARCH_TEXT = "SET_SEARCH_TEXT";
+const SET_PLACES = "SET_PLACES";
+
+const reducer = (state, action) => {
+  console.log({ action });
+  if (action.type === SET_SEARCH_TEXT) {
+    const { searchText } = action.payload;
+    return {
+      ...state,
+      searchText,
+    };
+  }
+
+  if (action.type === SET_PLACES) {
+    const { places } = action.payload;
+    return {
+      ...state,
+      places,
+    };
+  }
+
+  return state;
+};
+
+export const SearchFormProvider = ({ children }) => {
+  const [searchFormState, dispatch] = useReducer(reducer, initialState);
+
+  const placesSet = (data) => {
+    dispatch({
+      type: SET_PLACES,
+      payload: { places: data },
+    });
+  };
+
+  const setText = (searchText) => {
+    dispatch({
+      type: SET_SEARCH_TEXT,
+      payload: {
+        searchText,
+      },
+    });
+  };
+
+  const clearText = () => {
+    setText("");
+  };
+
+  const value = { searchFormState, placesSet, setText, clearText };
+
+  return (
+    <SearchFormContext.Provider value={value}>
+      {children}
+    </SearchFormContext.Provider>
+  );
+};
