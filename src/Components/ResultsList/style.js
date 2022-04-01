d2a97139c1f9447ee63402a767bdb2130e90b1e9
Indraan S. Toor <indraantoor@gmail.com>
2022-04-01 12:20:29 +0530

    Refactor: clean up css in results list

--- src/Components/ResultsList/style.js
+++ src/Components/ResultsList/style.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const ResultsListWrapper = styled.div`
-  // background-color: green;
   padding-top: 10px;
   display: flex;
   justify-content: center;
@@ -13,12 +12,8 @@ export const ResultsListContainer = styled.div`
   padding: 10px;
   max-height: 250px;
   overflow: auto;
-  // transition: height 2s ease-in;
-  //   border: solid 3px #ede6e3;
   border: none;
   border-radius: 10px;
-
-  //   box-shadow: 0 0 10px #dadad9;
   box-shadow: 0 0 6px #757780;
 
   &::-webkit-scrollbar {
